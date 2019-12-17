364acaed028013c4ea7edac8dcfeb53c510e10d3
rahul <rahul@gmail.com>
2019-12-17 16:18:23 +0530

    laravel private  chat with mysql

---
+++ resources/views/home.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="user-wrapper">
+                    <ul class="users">
+                        @foreach($users as $user)
+                            <li class="user" id="{{ $user->id }}">
+                                {{--will show unread count notification--}}
+                              <?php
+                              /*
+                                @if($user->unread)
+                                    <span class="pending">{{ $user->unread }}</span>
+                                @endif
+                                */
+                                ?>
+                                <div class="media">
+                                    <div class="media-left">
+
+                                    </div>
+
+                                    <div class="media-body">
+                                        <p class="name">{{ $user->name }}</p>
+                                        <p class="email">{{ $user->email }}</p>
+                                    </div>
+                                </div>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-md-8" id="messages">
+
+            </div>
+        </div>
+    </div>
+@endsection
